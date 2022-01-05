d2688875fc635665ac2be5c69f36a5f7b15d79dd
lg519 <lg519@ic.ac.uk>
2022-01-05 14:48:59 +0000

    added cosine function that computes n+1 terms of taylor expansion of cos(x)

--- Tick1q/Program.fs
+++ Tick1q/Program.fs
@@ -4,54 +4,73 @@
 
 // top-level subfunctions of polarToCartesianApprox (if any)
 
+// computes n! = n * (n-1) * ... * 2 * 1
+let fact n =
+    if n = 0 then
+        1.0
+    else
+        List.reduce (*) [ 1.0 .. float n ]
+
+// computes the first n+1 terms of taylor expansion of cos(x)
+let cosine (n: int) (x: float) =
+    let term (i: int) =
+        ((float -1) ** i * x ** (float (2 * i)))
+        / fact (2 * i)
+
+    List.map term [ 0 .. n ] |> List.reduce (+)
+
+
 /// answer to Tick1
 // the header given here is correct.
-let polarToCartesianApprox (r,theta) n = 
-    failwithf "Tick1 not yet implemented" // replace this line with your top-level implementation
+let polarToCartesianApprox (r, theta) n = failwithf "Tick1 not yet implemented" // replace this line with your top-level implementation
 
 
 //--------------------testbench code - DO NOT CHANGE-----------------------------//
 
 /// used to make generate testbench data
 let testInputs =
-    let testPolarCoords = List.allPairs [1.;2.] [1.;2.]
-    List.allPairs testPolarCoords [0;1;2;3;10]
+    let testPolarCoords = List.allPairs [ 1.; 2. ] [ 1.; 2. ]
+    List.allPairs testPolarCoords [ 0; 1; 2; 3; 10 ]
 
 /// data showing correct results generated with model answer and given here
 let testBenchData =
-    [
-        ((1.0, 1.0), 0, (1.0, 0.0))       
-        ((1.0, 2.0), 0, (1.0, 0.0))        
-        ((2.0, 1.0), 0, (2.0, 0.0))        
-        ((2.0, 2.0), 0, (2.0, 0.0))        
-        ((1.0, 1.0), 1, (1.0, 1.0))        
-        ((1.0, 2.0), 1, (1.0, 2.0))        
-        ((2.0, 1.0), 1, (2.0, 2.0))        
-        ((2.0, 2.0), 1, (2.0, 4.0))        
-        ((1.0, 1.0), 2, (0.5, 1.0))        
-        ((1.0, 2.0), 2, (-1.0, 2.0))        
-        ((2.0, 1.0), 2, (1.0, 2.0))        
-        ((2.0, 2.0), 2, (-2.0, 4.0))        
-        ((1.0, 1.0), 3, (0.5, 0.8333333333))        
-        ((1.0, 2.0), 3, (-1.0, 0.6666666667))        
-        ((2.0, 1.0), 3, (1.0, 1.666666667))        
-        ((2.0, 2.0), 3, (-2.0, 1.333333333))        
-        ((1.0, 1.0), 10, (0.5403023038, 0.8414710097))        
-        ((1.0, 2.0), 10, (-0.4161552028, 0.9093474427))        
-        ((2.0, 1.0), 10, (1.080604608, 1.682942019))        
-        ((2.0, 2.0), 10, (-0.8323104056, 1.818694885))
-    ]
+    [ ((1.0, 1.0), 0, (1.0, 0.0))
+      ((1.0, 2.0), 0, (1.0, 0.0))
+      ((2.0, 1.0), 0, (2.0, 0.0))
+      ((2.0, 2.0), 0, (2.0, 0.0))
+      ((1.0, 1.0), 1, (1.0, 1.0))
+      ((1.0, 2.0), 1, (1.0, 2.0))
+      ((2.0, 1.0), 1, (2.0, 2.0))
+      ((2.0, 2.0), 1, (2.0, 4.0))
+      ((1.0, 1.0), 2, (0.5, 1.0))
+      ((1.0, 2.0), 2, (-1.0, 2.0))
+      ((2.0, 1.0), 2, (1.0, 2.0))
+      ((2.0, 2.0), 2, (-2.0, 4.0))
+      ((1.0, 1.0), 3, (0.5, 0.8333333333))
+      ((1.0, 2.0), 3, (-1.0, 0.6666666667))
+      ((2.0, 1.0), 3, (1.0, 1.666666667))
+      ((2.0, 2.0), 3, (-2.0, 1.333333333))
+      ((1.0, 1.0), 10, (0.5403023038, 0.8414710097))
+      ((1.0, 2.0), 10, (-0.4161552028, 0.9093474427))
+      ((2.0, 1.0), 10, (1.080604608, 1.682942019))
+      ((2.0, 2.0), 10, (-0.8323104056, 1.818694885)) ]
 /// test testFun with testData to see whether actual results are the same as
 /// expected results taken from testData
 let testBench testData testFun =
     let closeTo f1 f2 = abs (f1 - f2) < 0.000001
-    let testItem fn (coords, n, (expectedX,expectedY) as expected) =
-        let actualX,actualY as actual = testFun coords n
-        if not (closeTo actualX expectedX) || not (closeTo actualY expectedY) then
-            printfn "Error: coords=%A, n=%d, expected result=%A, actual result=%A"coords n expected actual
+
+    let testItem fn (coords, n, (expectedX, expectedY) as expected) =
+        let actualX, actualY as actual = testFun coords n
+
+        if
+            not (closeTo actualX expectedX)
+            || not (closeTo actualY expectedY)
+        then
+            printfn "Error: coords=%A, n=%d, expected result=%A, actual result=%A" coords n expected actual
             1
         else
             0
+
     printfn "Starting tests..."
     let numErrors = List.sumBy (testItem testFun) testData
     printfn "%d tests Passed %d tests failed." (testData.Length - numErrors) numErrors
