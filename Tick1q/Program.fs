e5cffc0be8240e9267bd7702168fc264eef65301
lg519 <lg519@ic.ac.uk>
2022-01-17 08:59:10 +0000

    code refactoring

--- Tick1q/Program.fs
+++ Tick1q/Program.fs
@@ -11,54 +11,29 @@ let fact n =
     else
         List.reduce (*) [ 1.0 .. float n ]
 
-/// computes the number of odd numbers in range [0,R]
-let number_of_odd R =
-    let N = R + 1
-
-    if N % 2 = 0 then
-        let odd_numbers = N / 2
-        odd_numbers
-    else
-        let odd_numbers = ((N - 1) / 2)
-        odd_numbers
-
-/// computes the number of even numbers in range [0,R]
-let number_of_even R =
-    let N = R + 1
-    N - number_of_odd R
-
+/// computes the i-th term of the taylor expansion for cos(x) or sin(x)
+let term (x: float) (i: int) =
+    ((float -1) ** i * x ** (float (i))) / fact (i)
 
 /// computes the taylor expansion of cos(x) up to order n
 let cosine (n: int) (x: float) =
-    let number_of_terms = number_of_even n
+    let lst = [ 0 .. 2 .. n ]
 
-    let term (i: int) =
-        ((float -1) ** i * x ** (float (2 * i)))
-        / fact (2 * i)
-
-    let last_i_in_list = number_of_terms - 1
-    let i_list = [ 0 .. last_i_in_list ]
-
-    if i_list = [] then
+    if lst = [] then
         float 0
     else
-        List.map term i_list |> List.reduce (+)
+        List.map (term x) lst |> List.reduce (+)
+
+
 
 /// computes the taylor expansion of sin(x) up to order n
 let sine (n: int) (x: float) =
-    let number_of_terms = number_of_odd n
+    let lst = [ 1 .. 2 .. n ]
 
-    let term (i: int) =
-        (float -1) ** i * x ** (float (2 * i + 1))
-        / fact (2 * i + 1)
-
-    let last_i_in_list = number_of_terms - 1
-    let i_list = [ 0 .. last_i_in_list ]
-
-    if i_list = [] then
+    if lst = [] then
         float 0
     else
-        List.map term i_list |> List.reduce (+)
+        List.map (term x) lst |> List.reduce (+)
 
 
 
@@ -66,11 +41,8 @@ let sine (n: int) (x: float) =
 // the header given here is correct.
 let polarToCartesianApprox (r, theta) n =
 
-    let cosine_n = cosine n
-    let sine_n = sine n
-
-    let x = r * (cosine_n theta)
-    let y = r * (sine_n theta)
+    let x = r * (cosine n theta)
+    let y = r * (sine n theta)
 
     (x, y)
 
